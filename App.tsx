908d96f78803d291ce6820317f42e26ffd3e8fd3
Margarita Petrova <margarita.hr.petrova@gmail.com>
2024-10-29 15:44:44 +0200

    Add delete button

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from "react-native";
 import { theme } from "./theme";
 
 export default function App() {
+  const handleDelete = () => {
+    Alert.alert("Are you sure you want to delete this?", "It will be gone for good", [
+      { text: "Yes", onPress: () => console.log("ok, deleting"), style: "destructive" },
+      { text: "Cancel", style: "cancel" },
+    ]);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.itemContainer}>
         <Text style={styles.itemText}>Coffee</Text>
+        <TouchableOpacity style={styles.button} onPress={handleDelete} activeOpacity={0.8}>
+          <Text style={styles.buttonText}>Delete</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -22,6 +32,20 @@ const styles = StyleSheet.create({
     borderBottomColor: theme.colorCerulian,
     paddingHorizontal: 8,
     paddingVertical: 16,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   itemText: { fontSize: 18, fontWeight: "200" },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
+  },
 });
