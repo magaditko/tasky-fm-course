6099de7f5c93fa3865eec41c21c535befc85b59a
Margarita Petrova <margarita.hr.petrova@gmail.com>
2024-10-29 16:11:26 +0200

    add completed prop and styles to ShoppingListItem

--- App.tsx
+++ App.tsx
@@ -6,8 +6,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <ShoppingListItem name="Coffee" />
-      <ShoppingListItem name="Tea" />
-      <ShoppingListItem name="Filters" />
+      <ShoppingListItem name="Tea" isCompleted />
+      <ShoppingListItem name="Filters" isCompleted />
     </View>
   );
 }
